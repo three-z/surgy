cceb5cd5e982177265931f5b89a51ce2fba71369
Zzz <zzz29ru@gmail.com>
2018-05-28 18:20:44 +0300

    item, catalog menu

--- js/scripts.js
+++ js/scripts.js
@@ -73,6 +73,44 @@ $(document).ready(function() {
 		]
 	});
 
+	$('.catalog-thumbs__list').slick({
+		autoplay: false,
+		dots: false,
+		arrows: true,
+		infinite: true,
+		speed: 500,
+		vertical: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 840,
+				settings: {
+					vertical: false
+				}
+			},
+			{
+				breakpoint: 560,
+				settings: {
+					vertical: false,
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 440,
+				settings: {
+					vertical: false,
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+	$('.catalog-thumbs__item').on('click', function(e) {
+		e.preventDefault();
+		$('.catalog-item__big-image img').attr('src', $(this).find('img').data('big'));
+	});
+
 	$('.page .range-slider').slider({
 		range: true,
 		min: 0,
@@ -88,6 +126,26 @@ $(document).ready(function() {
 	$('.catalog-filter__open').on('click', function(e) {
 		e.preventDefault();
 		$(this).parents('.catalog-filter').find('.catalog-filter__form').toggleClass('catalog-filter__form--opened');
+	});
+
+	$('.middle-nav__button a').on('click', function(e) {
+		e.preventDefault();
+		$(this).parents('.middle-nav').toggleClass('middle-nav--opened');
+		if ($('.middle-nav').hasClass('middle-nav--opened')) {
+			$('<div class="middle-nav__overlay" />').appendTo('.page').mousedown(function(e) {
+				var clicked = $(e.target);
+				if (clicked.is('.middle-nav') || clicked.parents().is('.middle-nav')) {
+					return;
+				}
+				else {
+					$('.middle-nav').toggleClass('middle-nav--opened');
+					$('.middle-nav__overlay').remove();
+				}
+			});
+		}
+		else {
+			$('.middle-nav__overlay').remove();
+		}
 	});
 
 	$('.header-user__item--login > a').on('click', function(e) {
@@ -108,5 +166,20 @@ $(document).ready(function() {
 		else {
 			$('.header-login__overlay').remove();
 		}
+	});
+
+	$('.middle-nav__item').each(function() {
+		if ($(this).find('.middle-nav__list').length > 0) {
+			$(this).toggleClass('middle-nav__item--has-sublist');
+		}
+	});
+
+	$('.middle-nav__item--has-sublist').hover(function () {
+	    $(this).parents('.middle-nav').toggleClass('middle-nav--parent-hovered');
+	});
+
+	$('.catalog-item__count input').spinner({
+		min: 1,
+		max: 99
 	});
 });
