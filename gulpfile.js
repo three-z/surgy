f8dae469724f6161be5108b8bfdd4a601b7e293e
Zzz <zzz29ru@gmail.com>
2018-05-29 14:01:32 +0300

    order pay

--- gulpfile.js
+++ gulpfile.js
@@ -55,8 +55,22 @@ gulp.task('html:build', function () {
 gulp.task('style:build', function () {
 	gulp.src(path.src.style)
 		.pipe(plumber())
-		.pipe(sass())
-		.pipe(prefixer())
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}))
+		.pipe(prefixer({
+			browsers: [
+				'Android 2.3',
+				'Android >= 4',
+				'Chrome >= 20',
+				'Firefox >= 24',
+				'Explorer >= 8',
+				'iOS >= 6',
+				'Opera >= 12',
+				'Safari >= 6'
+			],
+			cascade: false
+		}))
 		.pipe(gulp.dest(path.build.css))
 		.pipe(reload({stream: true}));
 });
